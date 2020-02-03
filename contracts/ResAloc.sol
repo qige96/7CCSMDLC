ce2761948b0ad2509db8b8110a6d1679e66f8197
Ricky <zrq96@163.com>
2020-02-03 23:27:00 +0000

    comply the conventions

--- contracts/ResAloc.sol
+++ contracts/ResAloc.sol
@@ -9,37 +9,37 @@ contract SimpleResourceAllocation3 {
         quotes = initQuotes;
     }
     
-    function request(uint res_id) public returns (bool success) {
-        require(res_id >= 0 && res_id < quotes.length, "Invalid resource Id!");
-        require(ownership[msg.sender][res_id] == false, "You have allocated this resource!");
-        if (quotes[res_id] > 0) {
-            ownership[msg.sender][res_id] = true;
-            quotes[res_id] -= 1;
-            success = true;
+    function request(uint _resId) public returns (bool _success) {
+        require(_resId >= 0 && _resId < quotes.length, "Invalid resource Id!");
+        require(ownership[msg.sender][_resId] == false, "You have allocated this resource!");
+        if (quotes[_resId] > 0) {
+            ownership[msg.sender][_resId] = true;
+            quotes[_resId] -= 1;
+            _success = true;
         } else {
-            success = false;
+            _success = false;
         }
     }
     
-    function release(uint res_id) public returns (bool success) {
-        require(res_id >= 0 && res_id < quotes.length, "Invalid resource Id!");
-        require(ownership[msg.sender][res_id] == true, "You dont't have this resource to release!");
-        ownership[msg.sender][res_id] = false;
-        quotes[res_id]++;
-        success = true;
+    function release(uint _resId) public returns (bool _success) {
+        require(_resId >= 0 && _resId < quotes.length, "Invalid resource Id!");
+        require(ownership[msg.sender][_resId] == true, "You dont't have this resource to release!");
+        ownership[msg.sender][_resId] = false;
+        quotes[_resId]++;
+        _success = true;
     }
     
     
-    function viewAllQuotes() public view returns (uint[] memory allQuotes) {
-        allQuotes = quotes;
+    function viewAllQuotes() public view returns (uint[] memory _allQuotes) {
+        _allQuotes = quotes;
     }
     
-    function viewMyResources() public view returns (bool[] memory myResources) {
+    function viewMyResources() public view returns (bool[] memory _myResources) {
         bool[] memory temp = new bool[](quotes.length);
         for (uint i = 0; i < quotes.length; i++) {
             temp[i] = ownership[msg.sender][i];
         }
-        myResources = temp;
+        _myResources = temp;
     }
     
 }
