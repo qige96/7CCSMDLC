3e1b6f18401ca53d3bfa4fda8ff86108f49146e7
Ricky <zrq96@163.com>
2020-02-09 13:41:07 +0000

    add comments

--- contracts/ResAloc.sol
+++ contracts/ResAloc.sol
@@ -1,14 +1,21 @@
-pragma solidity >=0.4.25;
+pragma solidity >=0.4.20 <=0.6.0;
+
 
 contract SimpleResourceAllocation3 {
 
-    uint[] public quotes;
+    // quotes of all resources, the index of the array represents the Id of that resource
+    uint[] private quotes; 
+    
+    // resource ownership of alll users
     mapping(address => mapping(uint => bool)) internal ownership;
     
     constructor (uint[] memory initQuotes) public {
         quotes = initQuotes;
     }
     
+    // request resource from system
+    // @param _resId:  unsigned integer - the Id of the resource wanted
+    // @return _success: bool - success status of this transaction
     function request(uint _resId) public returns (bool _success) {
         require(_resId >= 0 && _resId < quotes.length, "Invalid resource Id!");
         require(ownership[msg.sender][_resId] == false, "You have allocated this resource!");
@@ -21,6 +28,9 @@ contract SimpleResourceAllocation3 {
         }
     }
     
+    // release resource to the system
+    // @param _resId:  unsigned integer - the Id of the resource to be released
+    // @return _success: bool - success status of this transaction
     function release(uint _resId) public returns (bool _success) {
         require(_resId >= 0 && _resId < quotes.length, "Invalid resource Id!");
         require(ownership[msg.sender][_resId] == true, "You dont't have this resource to release!");
@@ -29,11 +39,14 @@ contract SimpleResourceAllocation3 {
         _success = true;
     }
     
-    
+    // query the quote of all available resources 
+    // @return _allQuotes: array - current quote of all resources
     function viewAllQuotes() public view returns (uint[] memory _allQuotes) {
         _allQuotes = quotes;
     }
     
+    // query my resources 
+    // @return _myResources: array - tell what resources I have had
     function viewMyResources() public view returns (bool[] memory _myResources) {
         bool[] memory temp = new bool[](quotes.length);
         for (uint i = 0; i < quotes.length; i++) {
