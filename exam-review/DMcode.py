ea4554a77c78d33aee7094148389fc0e96666e8b
Ricky Zhu <zrq96@163.com>
2020-05-29 16:21:55 +0800

    Create DMcode.py

---
+++ exam-review/DMcode.py
@@ -0,0 +1,10 @@
+'''
+Implementation of some algorithms in PNN,
+a suppliment of KCL PNN lecture notes
+
+@ author: Ricky Zhu
+@ email:  rickyzhu@foxmail.com
+'''
+from __future__ import print_function
+import numpy as np
+import pandas as pd
