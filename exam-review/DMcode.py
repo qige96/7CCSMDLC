f35af0833cd432b92ab715c497b60a65899b05d5
Ricky Zhu <zrq96@163.com>
2020-05-29 16:58:12 +0800

    Update DMcode.py

--- exam-review/DMcode.py
+++ exam-review/DMcode.py
@@ -8,3 +8,44 @@ a suppliment of KCL PNN lecture notes
 from __future__ import print_function
 import numpy as np
 import pandas as pd
+
+
+def metrics_from_confusion_matrix(mat, class_index):
+    '''
+    compute precision, recall, f_value from confusion matrix.
+                         predicted
+              -----------------------
+              |    |  a  |  b  |  c |
+              -----------------------
+              | a  | 88  |  10 |  2 | 
+       actual | b  | 14  |  40 |  6 |
+              | c  | 18  |  10 | 12 |
+              ----------------------- 
+        For class a, precision = 0.73, recall = 0.88, f_value = 0.4
+        Overall, success rate = 0.7
+        [from: lec 3 slide 97]
+    
+    Parameters
+    -----------
+    mat: ndarray/ matrix
+        confusion matrix of an supervised learning result
+    class_index: int
+        index of class name, start from 0
+    
+    Returns
+    --------
+    metrics: dict
+        {
+            precision: float,
+            recall:    float,
+            f_value:   float,
+            success_rate: float
+        }
+    
+    Examples
+    ---------
+        >>> matrix = np.matrix([[88,10,2], [14,40,6], [18,10,12]])
+        >>> metrics_from_confusion_matrix(matrix, 0)
+        {'precision': 0.73, 'recall': 0.88, 'f_value': 0.4, 'successs_rate': 0.7}
+    '''
+    pass
