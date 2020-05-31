7245f8ce2d3c46edeeca38a646a312f3bdc0fc0f
Ricky <zrq96@163.com>
2020-06-01 01:27:00 +0800

    add association rule

--- exam-review/DMcode.py
+++ exam-review/DMcode.py
@@ -701,7 +701,7 @@ def cos_similarity(vector1, vector2):
 #                 Association Rules
 # ======================================================
 
-def one_item_set(txs):
+def one_item_sets(txs):
     '''
     construct one-item set from a batch of transactions
     
@@ -719,7 +719,7 @@ def one_item_set(txs):
     --------
         from Lecure 6 Slide 12
         >>> txs = [['o','s'], ['m','o','w'], ['o','d'], ['o','d','s'], ['w','s']]
-        >>> one_item_set(txs)
+        >>> one_item_sets(txs)
         ['o', 's', 'm', 'w', 'd']
     '''
     one_set = []
@@ -729,66 +729,66 @@ def one_item_set(txs):
                 one_set.append(item)
     return one_set
 
-def two_item_set(one_set, txs):
+def two_item_sets(one_sets, txs):
     '''
     construct two-item set from one-item set
     
     Parameters
     ----------
-    one_set: list
-        one-item set
+    one_sets: list
+        one-item sets
     txs: list
         list of transactions that is a list of items
 
     Returns
     -------
-    two_set: list
-        list of two-item set
+    two_sets: list
+        list of two-item sets
 
     Examples
     --------
         from Lecure 6 Slide 13
         >>> txs = [['o','s'], ['m','o','w'], ['o','d'], ['o','d','s'], ['w','s']]
-        >>> one_set = ['o', 's', 'm', 'w', 'd']
-        >>> two_item_set(one_set, txs)
+        >>> one_sets = ['o', 's', 'm', 'w', 'd']
+        >>> two_item_sets(one_sets, txs)
         [('o', 's'), ('o', 's'), ('o', 'm'), ('o', 'w'), ('o', 'd'), ('o', 'd'), ('s', 'w'), ('s', 'd'), ('m', 'w')]
     '''
-    two_set = []
+    two_sets = []
     from itertools import combinations_with_replacement
-    for tup in combinations_with_replacement(one_set, 2):
+    for tup in combinations_with_replacement(one_sets, 2):
         if len(set(tup)) == 2:
             for tx in txs:
                 if (tup[0] in tx) and (tup[1] in tx):
-                    two_set.append(tup)
-    return two_set
+                    two_sets.append(tup)
+    return two_sets
 
-def three_item_set(two_set, txs):
+def three_item_sets(two_sets, txs):
     '''
     construct three-item set from one-item set
     
     Parameters
     ----------
-    two_set: list
-        two-item set
+    two_sets: list
+        two-item sets
     txs: list
         list of transactions that is a list of items
 
     Returns
     -------
-    three_set: list
-        list of three-item set
+    three_sets: list
+        list of three-item sets
 
     Examples
     --------
         from Lecure 6 Slide 13
         >>> txs = [['o','s'], ['m','o','w'], ['o','d'], ['o','d','s'], ['w','s']]
-        >>> two_set = [('o','s'), ('o','d')]
-        >>> three_item_set(two_set, txs)
+        >>> two_sets = [('o','s'), ('o','d')]
+        >>> three_item_sets(two_sets, txs)
         [('o', 's', 'd')]
     '''
-    three_set = []
+    three_sets = []
     items = []
-    for i in two_set:
+    for i in two_sets:
         for k in i:
             if k not in items:
                 items.append(k)
@@ -798,29 +798,89 @@ def three_item_set(two_set, txs):
         if len(set(tup)) == 3:
             for tx in txs:
                 if (tup[0] in tx) and (tup[1] in tx) and (tup[2] in tx):
-                    three_set.append(tup)
-    return three_set
+                    three_sets.append(tup)
+    return three_sets
 
-def coverage():
+def coverage(item_set, txs):
     '''
     count the number of instances covered by the rule
 
-        [('o', 's'), ('o', 'w'), ('o', 'd'), ('s', 'w'), ('s', 'd'), ('w', 'd')]
-    '''
-    pass
+    Parameters
+    ----------
+    item_set: list
+        item set with any number of items
+    txs: list
+        list of transactions that is a list of items
 
-def support():
+    Returns
+    -------
+    count: int
+        coverage of a rule given a set of transaactions
+
+    Examples
+    --------
+        from Lecure 6 Slide 12
+        >>> txs = [['o','s'], ['m','o','w'], ['o','d'], ['o','d','s'], ['w','s']]
+        >>> set1 = ('o',); coverage(set1, txs)
+        4
+        >>> set2 = ('o','s'); coverage(set2, txs)
+        2
+    '''
+    count = 0
+    for tx in txs:
+        if set(tx).issuperset(item_set):
+            count += 1
+    return count
+
+def support(item_set, txs):
     '''
     calculate the proportion of instances covered by the rule
-    '''
-    pass
+    
+    Parameters
+    ----------
+    item_set: list
+        item set with any number of items
+    txs: list
+        list of transactions that is a list of items
 
-def confidence():
+    Returns
+    -------
+    proportion: float
+        proportion of a rule gain supported given a set of transaactions
+    '''
+    return coverage(item_set, txs) / len(txs)
+
+def confidence(rule, txs):
     '''
     calculate the proportion of instances that the rule predicts 
     correctly over all instances. Also called accuracy.
+
+    Parameters
+    ----------
+    rule: tuple
+        tuple of tuples, where tup[0] is antecedent and tup[1] is consequent
+    txs: list
+        list of transactions that is a list of items
+
+    Returns
+    -------
+    confid: float
+        confidence of the given rule
+
+    Examples
+    --------
+        from Lecure 6 Slide 16
+        >>> txs = [['o','s'], ['m','o','w'], ['o','d'], ['o','d','s'], ['w','s']]
+        >>> rule1 = (('o',), ('s',)); confidence(rule1, txs)
+        0.5
+        >>> rule2 = (('s',), ('o',)); confidence(rule2, txs)
+        0.6666666666666666
+        >>> rule3 = (('o',), ('d',)); confidence(rule3, txs)
+        0.5
+        >>> rule4 = (('d',), ('o',)); confidence(rule4, txs)
+        1.0
     '''
-    pass
+    return coverage(rule[0]+rule[1], txs) / coverage(rule[0], txs)
 
 
 if __name__ == '__main__':
